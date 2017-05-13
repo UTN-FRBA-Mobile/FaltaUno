ddbb9a96f27f48152af7466a286f06811a471a55
Jonathan <gastrelljonathan@gmail.com>
2017-05-13 20:40:34 -0300

    logueo y redirecciono, mas boton de cerrar sesion

--- app/src/main/java/com/faltauno/faltauno/Login_App.java
+++ app/src/main/java/com/faltauno/faltauno/Login_App.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.BundleCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -18,7 +19,7 @@ import com.facebook.login.widget.LoginButton;
  * Created by jonathan on 13/05/17.
  */
 
-public class FaltaUnoApp extends AppCompatActivity {
+public class Login_App extends AppCompatActivity {
 
     private LoginButton loginButton;
     private TextView textView;
@@ -40,22 +41,21 @@ public class FaltaUnoApp extends AppCompatActivity {
 
                 irAlMain();
 
-                textView.setText("Login sucess \n" +
-                loginResult.getAccessToken().getUserId() +
-                "\n" + loginResult.getAccessToken().getToken()
-                );
+
 
             }
 
             @Override
             public void onCancel() {
 
-                textView.setText("Login cancelled");
+
+                Toast.makeText(getApplicationContext(), R.string.cancel_login, Toast.LENGTH_SHORT).show();
 
             }
 
             @Override
             public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(),R.string.mensaje_error,Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -67,6 +67,7 @@ public class FaltaUnoApp extends AppCompatActivity {
     private void irAlMain() {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
 
 
     }
--- app/src/main/java/com/faltauno/faltauno/MainActivity.java
+++ app/src/main/java/com/faltauno/faltauno/MainActivity.java
@@ -1,12 +1,16 @@
 package com.faltauno.faltauno;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.design.widget.TabLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import com.facebook.FacebookSdk;
+import android.view.View;
+
+import com.facebook.AccessToken;
+import com.facebook.login.LoginManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (AccessToken.getCurrentAccessToken() == null) {
+            andaYLogueate();
+        }
 
         //Visualizacion del menu de la toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -40,11 +48,22 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
     }
 
+    private void andaYLogueate() {
+        Intent intent = new Intent(this, Login_App.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void logout(View view) {
+        LoginManager.getInstance().logOut();
+        andaYLogueate();
     }
 }
 
