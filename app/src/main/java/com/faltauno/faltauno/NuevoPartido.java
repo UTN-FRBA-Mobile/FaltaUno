91b56b44e49af68da5d767565bb2aecc39ae9822
vanesaviejo <vanesaviejo@gmail.com>
2017-05-15 20:30:14 -0300

    Subo clase nuevo partido

--- app/src/main/java/com/faltauno/faltauno/NuevoPartido.java
+++ app/src/main/java/com/faltauno/faltauno/NuevoPartido.java
@@ -3,20 +3,20 @@ package com.faltauno.faltauno;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link NuevoPartido.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link NuevoPartido#newInstance} factory method to
- * create an instance of this fragment.
- */
+import static com.faltauno.faltauno.R.id.numeroJugadores;
+
 public class NuevoPartido extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,8 +26,11 @@ public class NuevoPartido extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    public NumberPicker np;
+    public Spinner spinner;
 
-    private OnFragmentInteractionListener mListener;
+
+    //private OnFragmentInteractionListener mListener;
 
     public NuevoPartido() {
         // Required empty public constructor
@@ -41,7 +44,7 @@ public class NuevoPartido extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment NuevoPartido.
      */
-    // TODO: Rename and change types and number of parameters
+
     public static NuevoPartido newInstance(String param1, String param2) {
         NuevoPartido fragment = new NuevoPartido();
         Bundle args = new Bundle();
@@ -53,10 +56,27 @@ public class NuevoPartido extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
+
+
+//        String[] nums = new String[20];
+//        for(int i=0; i<nums.length; i++)
+//            nums[i] = Integer.toString(i);
+//
+//        np.setMinValue(1);
+//        np.setMaxValue(20);
+//        np.setWrapSelectorWheel(false);
+//        np.setDisplayedValues(nums);
+//        np.setValue(1);
+
+
         }
     }
 
@@ -64,18 +84,36 @@ public class NuevoPartido extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nuevo_partido, container, false);
+        View vista =inflater.inflate(R.layout.fragment_nuevo_partido, container, false);
+        //Asocio el NumberPicker con el xml
+        np = (NumberPicker) vista.findViewById (R.id.numeroJugadores);
+        //Le asigno vaor mínimo y máximo
+        np.setMinValue(1);
+        np.setMaxValue(20);
+        //Armo el Spinner de canchas
+        spinner = (Spinner) vista.findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(vista.getContext(),
+                R.array.canchas_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        return vista;
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
+/*
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
     }
+*/
 
-    @Override
-    public void onAttach(Context context) {
+   // @Override
+ /*   public void onAttach(Context context) {
         super.onAttach(context);
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
@@ -83,13 +121,24 @@ public class NuevoPartido extends Fragment {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
         }
-    }
+    }*/
 
-    @Override
+  /*  @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }*/
+
+    public static Fragment newInstance(String texto) {
+
+        NuevoPartido fragment = new NuevoPartido();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, texto);
+        fragment.setArguments(args);
+        return fragment;
     }
+
+
 
     /**
      * This interface must be implemented by activities that contain this
@@ -101,8 +150,10 @@ public class NuevoPartido extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
+/*    public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
-    }
+    }*/
+
+
 }
