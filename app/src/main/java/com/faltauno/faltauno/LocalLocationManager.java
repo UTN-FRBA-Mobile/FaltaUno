f5e5fd7de8e99a9b5fa2a31ef6f25b023b6ff76c
luchogonzalez <lucho.gonzalez125@gmail.com>
2017-07-03 20:56:03 -0300

    [roto] agrego clase localLocationManager para gestionar los permisos de geolocalizacion

---
+++ app/src/main/java/com/faltauno/faltauno/LocalLocationManager.java
@@ -0,0 +1,33 @@
+package com.faltauno.faltauno;
+
+import android.location.Location;
+
+/**
+ * Created by lgonzalez on 03-Jul-17.
+ * manages locationn permissions and interactions with the application
+ */
+
+public class LocationManager {
+    private static final LocationManager INSTANCE = new LocationManager();
+    private Gps gps;
+
+    private LocationManager() {};
+
+    public static LocationManager getInstance(){
+        if (INSTANCE == null) {
+            INSTANCE = new 
+            return INSTANCE;
+        }
+
+
+    }
+
+
+
+    }
+
+    public void showRequestPermission(){
+
+    }
+
+}
